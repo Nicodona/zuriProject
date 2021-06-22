0132cd5b6c79f634fa3963e9882c25c153268a00
njei nicodona <njeinicodona@gmail.com>
2021-06-22 18:46:31 +0100

    # updated the authentication provided a password recovery page using some attributes verification

--- Zuri/group/urls.py
+++ Zuri/group/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.signup, name='signup'),path('login/', views.login, name='login'),  path('success', views.success, name='success'),
+    path('logout/', views.logout, name='logout'),
+    path('reset_password/', views.reset_password, name='reset_password')
 ]
--- Zuri/group/views.py
+++ Zuri/group/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.views import View
+
 
 def signup(request):
     if request.method == 'POST':
@@ -46,3 +49,48 @@ def login(request):
 
 def success(request):
     return render(request, 'success.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signup')
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password1']
+        confirm_password = request.POST['password2']
+        email = request.POST['email']
+        username = request.POST['username']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                if User.objects.filter(email=email).exists():
+                    update = User.objects.get(username=username)
+                    update.password = make_password(password)
+                    update.save()
+                    return redirect('login')
+                else:
+                    messages.info(request, 'no matchng email related to ' + username + 'please provide a valid email')
+
+                    return redirect('reset_password')
+
+            else:
+                messages.info(request, 'we couldn\'t find any username relating to this')
+                return redirect('reset_password')
+
+        else:
+            messages.info(request, 'password mismatched please repeat')
+            return redirect('reset_password')
+    else:
+        return render(request, 'reset_password.html')
+
+
+# class RequestPassReset(View):
+#     def get(self, request):
+#         return render(request, 'reset_password.html')
+#
+#     def post(self, request):
+#         email = request.POST['email']
+#         context = {
+#             'values': request.POST
+#         }
+#         if not va
